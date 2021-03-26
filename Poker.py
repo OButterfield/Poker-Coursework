7f268128d1218cfbc88eef1bc98b8336c9d376ec
fr3shwat3r <oliverb@thebutterfields.org>
2021-03-26 15:37:07 +0000

    Created and implemented the hand comparison function

--- Poker.py
+++ Poker.py
@@ -48,6 +48,7 @@ p1=player("Bob")
 p2=player("Alice")
 newgame = game(0,p1,p2)
 
+
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -139,12 +140,12 @@ class main_screen(tk.Frame):
 
     def onePlayerStart(self):
         self.controller.playerNum = 1 
-        self.controller.show_frame(difficulty_screen)
+        self.controller.show_frame(difficulty_screen)       #Called by the One player button
 
     
     def twoPlayerStart(self):
         self.controller.playerNum = 2 
-        self.controller.show_frame(game_screen)
+        self.controller.show_frame(game_screen)             #Called by the Two player button
 
 
 class difficulty_screen(tk.Frame):
@@ -187,9 +188,9 @@ class game_screen(tk.Frame):
         self.columnconfigure(0,weight = 1)     
         self.columnconfigure(8,weight = 1)
        
-        self.background = tk.Canvas(self, width = 820, height = 392, bd = 0, highlightthickness = 0, bg = "black")  #This is the top middle image
+        self.background = tk.Canvas(self, width = 820, height = 392, bd = 0, highlightthickness = 0, bg = "black")  
         self.background.grid(row=3,column=2, rowspan = 2, columnspan = 5)
-        global table
+        global table                                                            #This is the background poker table
         table = ImageTk.PhotoImage(Image.open("Poker_table.png"))
         self.background.create_image(0,0,anchor="nw", image = table)
 
@@ -252,7 +253,7 @@ class game_screen(tk.Frame):
         
         self.boardID1 = self.background.create_image(200,196,image = self.back)
         self.boardID2 = self.background.create_image(305,196,image = self.back)
-        self.boardID3 = self.background.create_image(410,196,image = self.back)
+        self.boardID3 = self.background.create_image(410,196,image = self.back)     #All the board cards
         self.boardID4 = self.background.create_image(515,196,image = self.back)
         self.boardID5 = self.background.create_image(620,196,image = self.back)
 
@@ -266,11 +267,11 @@ class game_screen(tk.Frame):
         self.button_confirm.grid_forget() # hide the buttons
 
         if self.bet1.get().isdigit():
-            self.controller.amount = int(self.bet1.get())
+            amountCheck = int(self.bet1.get())
 
-            #previousBet = self.controller.amount
             if self.controller.playerGo == 1:
-                if self.controller.amount > 0 and self.controller.amount <= self.controller.maxBet - newgame.p1.totalBet and self.controller.amount <= int(newgame.p1.money) and self.controller.amount > self.controller.previousBet:  
+                if amountCheck > 0 and amountCheck <= self.controller.maxBet - newgame.p1.totalBet and amountCheck <= int(newgame.p1.money) and amountCheck > self.controller.previousBet:  
+                    self.controller.amount = amountCheck
                     newgame.p1.totalBet += self.controller.amount
                     self.controller.previousBet = self.controller.amount  
                     newgame.pot += self.controller.amount
@@ -337,15 +338,9 @@ class game_screen(tk.Frame):
         print("Player " + str(self.controller.playerGo) + " folded")
         if self.controller.playerGo == 1:
             newgame.p2.money += newgame.pot
-            self.money2.grid_forget()
-            self.money2 = tk.Label(self, text = newgame.p2.money, font = font1, fg = "blue", height = 3, width = 20, bg = "black")
-            self.money2.grid(column = 8, row = 1, sticky = "nsew")              #updating the players money
             self.newHand()    # Move onto next hand
         else:
             newgame.p1.money += newgame.pot
-            self.money1.grid_forget()
-            self.money1 = tk.Label(self, text = newgame.p1.money, font = font1, fg = "blue", height = 3, width = 20, bg = "black")
-            self.money1.grid(column = 0, row = 1, sticky = "nsew")              #updating the players money
             self.newHand()    # Move onto next hand
 
     def newHand(self):
@@ -353,6 +348,20 @@ class game_screen(tk.Frame):
         print(" ")
         print("New Hand")
         print(" ")
+
+
+        self.money1.grid_forget()
+        self.money2.grid_forget()
+
+        self.money1 = tk.Label(self, text = newgame.p1.money, font = font1, fg = "blue", height = 3, width = 20, bg = "black")
+        self.money2 = tk.Label(self, text = newgame.p2.money, font = font1, fg = "blue", height = 3, width = 20, bg = "black")
+        
+        #updating players money
+
+        self.money1.grid(column = 0, row = 1, sticky = "nsew")
+        self.money2.grid(column = 8, row = 1, sticky = "nsew")
+
+
 
         newgame.pot = 0
         self.pot_label.grid_forget()
@@ -460,16 +469,13 @@ class game_screen(tk.Frame):
             elif self.controller.betStage == 2:
                 self.river()
             else:
-                pass
-                # run card ranking function when made.
+                self.compare()                            #compare the 2 players hands and output a winner
 
     def callLabelForget(self):
         self.callLabel.grid_forget()    
 
 
     def check(self):
-        self.rank(newgame.p1.hand,newgame.board)
-        exit()
         self.bet1.grid_forget()
         self.button_confirm.grid_forget()
         if self.controller.amount == None:
@@ -479,7 +485,7 @@ class game_screen(tk.Frame):
 
             elif self.controller.playerGo == 1 and self.controller.handNum % 2 == 0:  # if is is player 1 and they are going second 
                 self.controller.playerGo = 2                                          # - ie betting round over
-
+  
                 if self.controller.betStage == 0:
                     self.flop()
                 elif self.controller.betStage == 1:                                     # figures out which round and show cards
@@ -487,8 +493,7 @@ class game_screen(tk.Frame):
                 elif self.controller.betStage == 2:
                     self.river()
                 else:
-                    pass
-                    # run card ranking function when made.
+                    self.compare()                            #compare the 2 players hands and output a winner
 
             elif self.controller.playerGo == 2 and self.controller.handNum % 2 == 0:    #player twos go and they are acting first
                 self.controller.playerGo = 1
@@ -502,19 +507,18 @@ class game_screen(tk.Frame):
                 elif self.controller.betStage == 2:
                     self.river()
                 else:
-                    pass
-                    #hand comparison function
+                    self.compare()                            #compare the 2 players hands and output a winner
         
         else:
             self.checkLabel = tk.Label(self, text = "A bet has been made", font = font2, fg = "blue", height = 3, width = 20, bg = "black")
             self.checkLabel.grid(column = 8, row = 5, sticky = "nsew")
             self.controller.after(2000, self.hideCheckLabel)
 
-    def hideCheckLabel(self):
+    def hideCheckLabel(self):               #hide the label if player tries to check when a bet has been made
         self.checkLabel.grid_forget()
 
 
-    def getValue(self,card):
+    def getValue(self,card):        #to use for the sort command in rank
         return card.value
 
 
@@ -525,24 +529,144 @@ class game_screen(tk.Frame):
         for i in board:
             allCards.append(i)
         allCards.sort(key=self.getValue)
-        combinations = itertools.combinations(allCards,5)
-        for i in combinations:
-            print(i)
-        exit()
-        highestScore = [0,0]
-        for x in combinations:
-            currentScore = score(x)
-            if highestScore[0] < currentScore[0]:
+        combination = itertools.combinations(allCards,5)
+        highestScore = [0,0]    #value to compare each combination against
+        for x in combination:
+            currentScore = self.score(x)        #score of the current 5 card combination
+            if highestScore[0] < currentScore[0]:   
                 for i in range(2):
-                    highestScore[i] = currentScore[i]
+                    highestScore[i] = currentScore[i]   #setting highestScore to be currentScore
             elif highestScore[0] == currentScore[0] and highestScore[1] < currentScore[1]:
                 for i in range(2):
-                    highestScore[i] = currentScore[i]
-        
+                    highestScore[i] = currentScore[i]   #setting highestScore to be currentScore
+        print(highestScore)
+        return highestScore
     
 
-    #def score(self):
+    def score(self,set):
+        straight1 = False
+        straight2 = False
+        flush = False
 
+        if set[0].value == set[1].value - 1 == set[2].value - 2 == set[3].value - 3 == set[4].value - 4:
+            straight1 = True
+
+        if set[0].value == set[1].value - 1 == set[2].value - 2 == set[3].value - 3 == set[4].value - 12:
+            straight2 = True
+
+        if set[0].suit == set[1].suit == set[2].suit == set[3].suit == set[4].suit:
+            flush = True
+
+        #Royal Flush
+        if straight1 and flush and set[4].value == 14:
+            return [9,0]
+
+        #Straight Flush
+        elif straight1 and flush:
+            return [8,set[4].value]
+        elif straight2 and flush:
+            return [8,set[3].value]
+
+        #Four of a kind
+        elif set[4].value == set[3].value == set[2].value == set[1].value:
+            return [7,(set[4].value)*10 + set[0].value]
+        elif set[3].value == set[2].value == set[1].value == set[0].value:
+            return [7,(set[3].value)*10 + set[4].value]
+
+        #Full House
+        elif set[4].value == set[3].value == set[2].value and set[1].value == set[0].value:
+            return [6,(set[4].value)* 10 + set[1].value]
+        elif set[2].value == set[1].value == set[0].value and set[4].value == set[3].value:
+            return [6,(set[2].value)* 10 + set[4].value]
+        
+        #Flush
+        elif flush:
+            return[5,(set[4].value)* 10000 + (set[3].value)* 1000 + (set[2].value)*100 + (set[1].value)* 10 + (set[0].value)]
+        
+        #Straight
+        elif straight1:
+            return[4,set[4].value]
+        elif straight2:
+            return[4, set[3].value]
+        
+        #Three of a kind
+        elif set[4].value == set[3].value == set[2].value:
+            return[3,(set[4].value)* 100 + (set[1].value)* 10 + (set[0].value)]
+        elif set[3].value == set[2].value == set[1].value:
+            return[3,(set[3].value)* 100 + (set[4].value)* 10 + (set[0].value)]
+        elif set[2].value == set[1].value == set[0].value:
+            return[3,(set[2].value)* 100 + (set[4].value)* 10 + (set[3].value)]
+        
+        #Two Pair
+        elif set[4].value == set[3].value and set[2].value == set[1].value:
+            return[2, (set[4].value)* 100 + (set[2].value)*10 + set[0].value]
+        elif set[4].value == set[3].value and set[1].value == set[0].value:
+            return[2, (set[4].value)* 100 + (set[1].value)*10 + set[2].value]
+        elif set[3].value == set[2].value and set[1].value == set[0].value:
+            return[2, (set[3].value)* 100 + (set[1].value)*10 + set[4].value]
+        
+        #One Pair
+        elif set[4].value == set[3].value:
+            return[1,(set[4].value)* 1000 + (set[2].value)* 100 + (set[1].value)*10 + (set[0].value)]
+        elif set[3].value == set[2].value:
+            return[1,(set[3].value)* 1000 + (set[4].value)* 100 + (set[1].value)*10 + (set[0].value)]
+        elif set[2].value == set[1].value:
+            return[1,(set[2].value)* 1000 + (set[4].value)* 100 + (set[3].value)*10 + (set[0].value)]
+        elif set[1].value == set[0].value:
+            return[1,(set[1].value)* 1000 + (set[4].value)* 100 + (set[3].value)*10 + (set[2].value)]
+        
+        #High Card
+        else:
+            return [0,(set[4].value)* 10000 + (set[3].value)* 1000 + (set[2].value)*100 + (set[1].value)* 10 + set[0].value]
+
+
+
+    def compare(self):
+        p1Score = self.rank(newgame.p1.hand, newgame.board)
+        p2Score = self.rank(newgame.p2.hand, newgame.board)
+        if p1Score[0] > p2Score[0]:
+            self.win1_label = tk.Label(self, text = str(newgame.p1.name) + " won the hand", font = font1, fg = "yellow", bg = "black")
+            self.win1_label.grid(column = 3, row = 0, columnspan = 2, sticky = "nsew")
+            newgame.p1.money += newgame.pot
+
+
+            self.controller.after(2000, self.hideWin1Label)
+            self.newHand()
+
+        elif p1Score[0] == p2Score[0] and p1Score[1] > p2Score[1]:
+            self.win1_label = tk.Label(self, text = str(newgame.p1.name)+ " won the hand", font = font1, fg = "yellow", bg = "black")
+            self.win1_label.grid(column = 3, row = 0, columnspan = 2, sticky = "nsew")
+            newgame.p1.money += newgame.pot
+
+            self.controller.after(2000, self.hideWin1Label)
+            self.newHand()
+       
+        elif p1Score == p2Score:
+            self.draw_label = tk.Label(self, text = "The hand was a draw", font = font1, fg = "yellow", bg = "black")
+            self.draw_label.grid(column = 3, row = 0, columnspan = 2, sticky = "nsew")
+            newgame.p1.money += newgame.p1.totalBet
+            newgame.p2.money += newgame.p2.totalBet
+
+            self.controller.after(2000, self.hideDrawLabel)
+            self.newHand()
+        else:
+            self.win2_label = tk.Label(self, text = str(newgame.p2.name) + " won the hand", font = font1, fg = "yellow", bg = "black")
+            self.win2_label.grid(column = 3, row = 0, columnspan = 2, sticky = "nsew")
+            newgame.p2.money += newgame.pot
+
+
+            self.controller.after(2000, self.hideWin2Label)
+            self.newHand()
+
+
+    def hideWin1Label(self):               #hide the  player 1 wins label 
+        self.win1_label.grid_forget()
+
+    def hideDrawLabel(self):               #hide the players have drawn label
+        self.draw_label.grid_forget()
+
+    def hideWin2Label(self):               #hide the  player 2 wins label
+        self.win2_label.grid_forget()
 
 
 
